eb101a43d083c42f857e0eb7cac46ee62333bbec
CALVINCE <calvince.ogwel@gmail.com>
2022-12-03 19:35:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 8);
+
+    if ( left > 0 ) {
+      dodger.style.left = `${left - 3}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 8);
+
+    if ( left < 180) {
+      dodger.style.left = `${left + 3}px`;
+    }
+  }
